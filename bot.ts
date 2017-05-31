9889ebd7484f652bdbde51d1e38b19ca01a6d205
Adrian Sampson <adrian@radbox.org>
2017-05-31 14:15:57 -0400

    Slack client library is untyped
    Can't find any up-to-date types for @slack/client. Looks like they're
    transitioning to TS, though:
    https://github.com/slackapi/node-slack-sdk/pull/352

--- bot.ts
+++ bot.ts
@@ -1,11 +1,11 @@
-import { RtmClient, CLIENT_EVENTS } from '@slack/client';
+const slack_client = require('@slack/client');
 
 const bot_token = process.env.SLACK_BOT_TOKEN || '';
 
-let rtm = new RtmClient(bot_token);
+let rtm = new slack_client.RtmClient(bot_token);
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData: any) => {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
