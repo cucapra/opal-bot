8d16a7ee969b17b9c6f81ae643fae28c1c2de4c2
Adrian Sampson <adrian@radbox.org>
2017-05-31 19:18:22 -0400

    Interfaces for start data

--- bot.ts
+++ bot.ts
@@ -4,9 +4,64 @@ const bot_token = process.env['SLACK_BOT_TOKEN'] || '';
 
 let rtm = new slack_client.RtmClient(bot_token);
 
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData: any) => {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+interface User {
+  id: string;
+  name: string;
+  prefs: { [key: string]: any };
+  created: number;
+  manual_presence: string;
+};
+
+interface Team {
+  id: string;
+  name: string;
+  email_domain: string;
+  domain: string;
+  msg_edit_window_mins: number;
+  prefs: { [key: string]: any };
+  icon: { [key: string]: any };
+  over_storage_limit: boolean;
+  approaching_msg_limit: false;
+  messages_count: number;
+  plan: string;
+  avatar_base_url: string;
+  over_integrations_limit: boolean;
+};
+
+interface Channel {
+  id: string;
+  name: string;
+  is_channel: boolean;
+  created: number;
+  creator: string;
+  is_archived: boolean;
+  is_general: boolean;
+  name_normalized: string;
+  is_shared: boolean;
+  is_org_shared: boolean;
+  has_pins: boolean;
+  is_member: boolean;
+  previous_names: string[];
+};
+
+interface RTMStartData {
+  ok: boolean;
+  self: User;
+  team: Team;
+  latest_event_ts: string;
+  channels: Channel[];
+  groups: object[];
+  ims: object[];
+  cache_ts: number;
+  users: object[];
+  url: string;
+  scopes: string[];
+  acceptedScopes: string[];
+};
+
+// Event handler for successful connection.
+rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (startData: RTMStartData) => {
+  console.log(`Logged in as ${startData.self.name} of team ${startData.team.name}`);
 });
 
 interface Message {
