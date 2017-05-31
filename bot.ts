8abf436df43fc337708ee128ffe2aab649c1abe1
Adrian Sampson <adrian@radbox.org>
2017-05-31 14:06:54 -0400

    Copy example from Slack docs

---
+++ bot.ts
@@ -0,0 +1,12 @@
+import { RtmClient, CLIENT_EVENTS } from '@slack/client';
+
+const bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+let rtm = new RtmClient(bot_token);
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+rtm.start();
