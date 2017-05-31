29d33f407c843afff3882b2249cb4fba650e560c
Adrian Sampson <adrian@radbox.org>
2017-05-31 14:33:01 -0400

    Handle messages

--- bot.ts
+++ bot.ts
@@ -1,12 +1,26 @@
 const slack_client = require('@slack/client');
 
-const bot_token = process.env.SLACK_BOT_TOKEN || '';
+const bot_token = process.env['SLACK_BOT_TOKEN'] || '';
 
 let rtm = new slack_client.RtmClient(bot_token);
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
 rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData: any) => {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+interface Message {
+  type: string;
+  channel: string;
+  user: string;
+  text: string;
+  ts: string;
+  source_team: string;
+  team: string;
+};
+
+rtm.on(slack_client.RTM_EVENTS.MESSAGE, (message: any) => {
+  console.log(`${message.user} sez ${message.text}`);
 });
 
 rtm.start();
