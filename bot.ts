640a50b9f0663467e1047988e1af2858c37fa220
Adrian Sampson <adrian@radbox.org>
2017-05-31 20:20:17 -0400

    A little bot library

--- bot.ts
+++ bot.ts
@@ -1,102 +1,16 @@
 import * as child_process from 'child_process';
+import { Bot, Message, RTMStartData } from './slackbot';
 const slack_client = require('@slack/client');
 
 const bot_token = process.env['SLACK_BOT_TOKEN'] || '';
 const status_chan = 'bot-status';
 
-let rtm = new slack_client.RtmClient(bot_token);
-
-interface User {
-  id: string;
-  name: string;
-  prefs: { [key: string]: any };
-  created: number;
-  manual_presence: string;
-};
-
-interface Team {
-  id: string;
-  name: string;
-  email_domain: string;
-  domain: string;
-  msg_edit_window_mins: number;
-  prefs: { [key: string]: any };
-  icon: { [key: string]: any };
-  over_storage_limit: boolean;
-  approaching_msg_limit: false;
-  messages_count: number;
-  plan: string;
-  avatar_base_url: string;
-  over_integrations_limit: boolean;
-};
-
-interface Channel {
-  id: string;
-  name: string;
-  is_channel: boolean;
-  created: number;
-  creator: string;
-  is_archived: boolean;
-  is_general: boolean;
-  name_normalized: string;
-  is_shared: boolean;
-  is_org_shared: boolean;
-  has_pins: boolean;
-  is_member: boolean;
-  previous_names: string[];
-};
-
-interface IM {
-  id: string;
-  created: number;
-  is_im: boolean;
-  is_org_shared: boolean;
-  user: string;
-  has_pins: false;
-  last_read: string;
-  latest: any;
-  unread_count: number;
-  unread_count_display: number;
-  is_open: boolean;
-};
-
-interface RTMStartData {
-  ok: boolean;
-  self: User;
-  team: Team;
-  latest_event_ts: string;
-  channels: Channel[];
-  groups: object[];
-  ims: IM[];
-  cache_ts: number;
-  users: object[];
-  url: string;
-  scopes: string[];
-  acceptedScopes: string[];
-};
-
-// State bits that should be come fields someday.
-let cur_channels: { [id: string]: Channel } = {};
-let cur_ims: { [id: string]: IM } = {};
-let status_channel_id: string | null = null;
+let bot = new Bot(bot_token);
 
 // Event handler for successful connection.
-rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (startData: RTMStartData) => {
+bot.rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED, (startData: RTMStartData) => {
   console.log(`logged in as ${startData.self.name} to ${startData.team.name}`);
 
-  for (let channel of startData.channels) {
-    cur_channels[channel.id] = channel;
-  }
-  for (let im of startData.ims) {
-    cur_ims[im.id] = im;
-  }
-
-  // Look for the status channel.
-  for (let channel of startData.channels) {
-    if (channel.name === status_chan && channel.is_member) {
-      status_channel_id = channel.id;
-    }
-  }
 });
 
 function git_summary(path: string): Promise<string> {
@@ -112,32 +26,31 @@ function git_summary(path: string): Promise<string> {
   });
 }
 
-rtm.on(slack_client.CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+bot.rtm.on(slack_client.CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  // Look for the status channel.
+  let status_channel_id: string | null = null;
+  for (let channel_id in bot.cur_channels) {
+    let channel = bot.cur_channels[channel_id];
+    if (channel.name === status_chan && channel.is_member) {
+      status_channel_id = channel.id;
+    }
+  }
+
   // Indicate that we've started.
   if (status_channel_id) {
     git_summary(__dirname).then((commit) => {
-      rtm.sendMessage(`:wave: @ ${commit}`, status_channel_id);
+      bot.rtm.sendMessage(`:wave: @ ${commit}`, status_channel_id);
     });
   }
 });
 
-interface Message {
-  type: string;
-  channel: string;
-  user: string;
-  text: string;
-  ts: string;
-  source_team: string;
-  team: string;
-};
-
-rtm.on(slack_client.RTM_EVENTS.MESSAGE, (message: any) => {
+bot.rtm.on(slack_client.RTM_EVENTS.MESSAGE, (message: Message) => {
   console.log(`${message.user} sez ${message.text}`);
 
   // Respond to private messages, just for fun.
-  if (cur_ims[message.channel]) {
-    rtm.sendMessage("hi!", message.channel);
+  if (bot.cur_ims[message.channel]) {
+    bot.rtm.sendMessage("hi!", message.channel);
   }
 });
 
-rtm.start();
+bot.start();
---
+++ slackbot.ts
@@ -0,0 +1,115 @@
+/**
+ * A helper for the Slack API for building bots.
+ */
+
+const slack_client = require('@slack/client');
+
+export interface User {
+  id: string;
+  name: string;
+  prefs: { [key: string]: any };
+  created: number;
+  manual_presence: string;
+};
+
+export interface Team {
+  id: string;
+  name: string;
+  email_domain: string;
+  domain: string;
+  msg_edit_window_mins: number;
+  prefs: { [key: string]: any };
+  icon: { [key: string]: any };
+  over_storage_limit: boolean;
+  approaching_msg_limit: false;
+  messages_count: number;
+  plan: string;
+  avatar_base_url: string;
+  over_integrations_limit: boolean;
+};
+
+export interface Channel {
+  id: string;
+  name: string;
+  is_channel: boolean;
+  created: number;
+  creator: string;
+  is_archived: boolean;
+  is_general: boolean;
+  name_normalized: string;
+  is_shared: boolean;
+  is_org_shared: boolean;
+  has_pins: boolean;
+  is_member: boolean;
+  previous_names: string[];
+};
+
+export interface IM {
+  id: string;
+  created: number;
+  is_im: boolean;
+  is_org_shared: boolean;
+  user: string;
+  has_pins: false;
+  last_read: string;
+  latest: any;
+  unread_count: number;
+  unread_count_display: number;
+  is_open: boolean;
+};
+
+export interface RTMStartData {
+  ok: boolean;
+  self: User;
+  team: Team;
+  latest_event_ts: string;
+  channels: Channel[];
+  groups: object[];
+  ims: IM[];
+  cache_ts: number;
+  users: object[];
+  url: string;
+  scopes: string[];
+  acceptedScopes: string[];
+};
+
+export interface Message {
+  type: string;
+  channel: string;
+  user: string;
+  text: string;
+  ts: string;
+  source_team: string;
+  team: string;
+};
+
+export class Bot {
+  public rtm: any;
+
+  public cur_channels: { [id: string]: Channel } = {};
+  public cur_ims: { [id: string]: IM } = {};
+  public status_channel_id: string | null = null;
+
+  /**
+   * Construct a bot by creating a Slack RTM client object and attach this
+   * bot's listeners.
+   */
+  constructor(token: string) {
+    this.rtm = new slack_client.RtmClient(token);
+
+    // Event handler for successful connection.
+    this.rtm.on(slack_client.CLIENT_EVENTS.RTM.AUTHENTICATED,
+                (startData: RTMStartData) => {
+      for (let channel of startData.channels) {
+        this.cur_channels[channel.id] = channel;
+      }
+      for (let im of startData.ims) {
+        this.cur_ims[im.id] = im;
+      }
+    });
+  }
+
+  start() {
+    this.rtm.start();
+  }
+}
