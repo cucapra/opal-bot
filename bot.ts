056665baa772c2ab3412aa0730a20791ca4f592c
Adrian Sampson <adrian@radbox.org>
2017-06-01 17:24:26 -0400

    Properly load from the database

--- bot.ts
+++ bot.ts
@@ -31,71 +31,16 @@ function git_summary(path: string): Promise<string> {
   });
 }
 
-/**
- * Handle a direct interaction.
- */
-async function interact(message: Message) {
-  let text = message.text;
-  let chan = message.channel;
-
-  let res = await wit_client.message(text, {});
-  console.log(`Wit parse: ${util.inspect(res, { depth: undefined })}`);
-
-  if (wit.getEntity(res, "greetings")) {
-    bot.send("hi!", chan);
-    return;
-  } else {
-    let intent = wit.entityValue(res, "intent");
-    if (intent === "show_calendar") {
-      bot.send("let's get your calendar!", chan);
-
-      // Do we already have a calendar URL for this user?
-      let user = getUser(message.user);
-      if (user.calendar_url) {
-        let url = user.calendar_url;
-        bot.send(`your calendar URL is ${url}`, chan);
-      } else {
-        bot.send("please paste your calendar URL", chan);
-        let url = (await bot.wait(chan)).text;
-        console.log(`getting calendar at ${url}`);
-        /*
-        let resp = await fetch(url);
-        let jcal = ical.parse(await resp.text());
-        console.log(jcal);
-        */
-        bot.send("thanks!", chan);
-        user.calendar_url = url;
-        users.update(user);
-      }
-
-      return;
-    } else if (intent === "schedule_meeting") {
-      bot.send("let's schedule a meeting!", chan);
-      return;
-    }
-  }
-
-  // Unhandled message.
-  bot.send(':confused: :grey_question:', chan);
-}
-
 interface User {
   slack_id: string;
   calendar_url?: string;
 }
 
-/**
- * Get a user from the database, or create it if it doesn't exist.
- */
-function getUser(slack_id: string): User {
-  let user = users.findOne({ slack_id });
-  if (user) {
-    return user as User;
-  } else {
-    let newUser = { slack_id };
-    users.insert(newUser);
-    return newUser;
-  }
+function openDB(): Promise<Loki> {
+  return new Promise((resolve, reject) => {
+    let db = new Loki(DB_NAME);
+    db.loadDatabase({}, () => resolve(db));
+  });
 }
 
 const wit_client = new Wit({
@@ -104,24 +49,93 @@ const wit_client = new Wit({
 
 const bot = new Bot(BOT_TOKEN);
 
-const db = new Loki(path.join(__dirname, DB_NAME));
-const users = db.addCollection("users");
+async function main() {
+  const db = await openDB();
+  const users = db.getCollection("users") || db.addCollection("users");
 
-bot.on("ready", async () => {
-  console.log(`I'm ${bot.self.name} on ${bot.team.name}`);
+  /**
+   * Handle a direct interaction.
+   */
+  async function interact(message: Message) {
+    let text = message.text;
+    let chan = message.channel;
 
-  // Indicate that we've started.
-  let status_channel = bot.channel(STATUS_CHAN);
-  if (status_channel) {
-    let commit = await git_summary(__dirname);
-    bot.send(`:wave: @ ${commit}`, status_channel.id);
+    let res = await wit_client.message(text, {});
+    console.log(`Wit parse: ${util.inspect(res, { depth: undefined })}`);
+
+    if (wit.getEntity(res, "greetings")) {
+      bot.send("hi!", chan);
+      return;
+    } else {
+      let intent = wit.entityValue(res, "intent");
+      if (intent === "show_calendar") {
+        bot.send("let's get your calendar!", chan);
+
+        // Do we already have a calendar URL for this user?
+        let user = getUser(message.user);
+        console.log("user is", user);
+        if (user.calendar_url) {
+          let url = user.calendar_url;
+          bot.send(`your calendar URL is ${url}`, chan);
+        } else {
+          bot.send("please paste your calendar URL", chan);
+          let url = (await bot.wait(chan)).text;
+          console.log(`getting calendar at ${url}`);
+          /*
+          let resp = await fetch(url);
+          let jcal = ical.parse(await resp.text());
+          console.log(jcal);
+          */
+          bot.send("thanks!", chan);
+          user.calendar_url = url;
+          users.update(user);
+          db.saveDatabase();
+        }
+
+        return;
+      } else if (intent === "schedule_meeting") {
+        bot.send("let's schedule a meeting!", chan);
+        return;
+      }
+    }
+
+    // Unhandled message.
+    bot.send(':confused: :grey_question:', chan);
   }
-});
 
-bot.onInit(async (message) => {
-  // A new private message.
-  console.log(`${message.user}: ${message.text}`);
-  interact(message);
-});
+  /**
+   * Get a user from the database, or create it if it doesn't exist.
+   */
+  function getUser(slack_id: string): User {
+    let user = users.findOne({ slack_id }) as User;
+    if (user) {
+      return user;
+    } else {
+      let newUser = { slack_id };
+      users.insert(newUser);
+      db.saveDatabase();
+      return newUser;
+    }
+  }
 
-bot.start();
+  bot.on("ready", async () => {
+    console.log(`I'm ${bot.self.name} on ${bot.team.name}`);
+
+    // Indicate that we've started.
+    let status_channel = bot.channel(STATUS_CHAN);
+    if (status_channel) {
+      let commit = await git_summary(__dirname);
+      bot.send(`:wave: @ ${commit}`, status_channel.id);
+    }
+  });
+
+  bot.onInit(async (message) => {
+    // A new private message.
+    console.log(`${message.user}: ${message.text}`);
+    interact(message);
+  });
+
+  bot.start();
+}
+
+main();
