adf71161c1eb03d7d32ebe4b6d22791f67716c82
Adrian Sampson <adrian@radbox.org>
2017-06-07 18:29:19 -0400

    A few more types

--- types/messenger-bot.d.ts
+++ types/messenger-bot.d.ts
@@ -9,10 +9,13 @@ interface Options {
 
 type Callback = (res: {}) => void | null;
 
-interface MessageEvent {
+interface Event {
   sender: { id: string };
   recipient: { id: string };
   timestamp: number;
+}
+
+interface MessageEvent extends Event {
   message: {
     mid: string;
     seq: number;
@@ -20,13 +23,29 @@ interface MessageEvent {
   };
 }
 
+interface PostbackEvent extends Event {
+  postback: {
+    payload: string;
+    referral: any;
+  };
+}
+
+interface Message {
+  // Either text or attachment must be set. For now, the types requre text.
+  text: string;
+  attachment?: any;
+  quick_reples?: any;
+  metadata?: any;
+}
+
 declare class Bot extends EventEmitter {
   constructor(opts: Options);
   getProfile(id: string, cb: Callback): void;
-  sendMessage(recipient: string, payload: { text: string }, cb: Callback): void;
+  sendMessage(recipient: string, payload: Message, cb: Callback): void;
   middleware(): (req: http.IncomingMessage, res: http.ServerResponse) => void;
 
   on(event: "message", listener: (event: MessageEvent) => void): this;
+  on(event: "postback", listener: (event: PostbackEvent) => void): this;
 }
 
 export = Bot;
