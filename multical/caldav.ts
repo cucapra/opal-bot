ad5db9eca59d77e02b06ee5b61f1ecdb84143e3a
Adrian Sampson <adrian@radbox.org>
2017-06-14 16:30:46 -0400

    A bunch of comments

--- multical/caldav.ts
+++ multical/caldav.ts
@@ -4,11 +4,64 @@ import * as ical from 'ical.js';
 import * as calendar from '../lib/calendar';
 import * as moment from 'moment';
 
+/**
+ * Encode a string using base64.
+ */
+function base64encode(s: string) {
+  return new Buffer(s).toString('base64');
+}
+
+/**
+ * Construct an `Authorization` header for HTTP Basic Auth.
+ */
+function basicauth(username: string, password: string): string {
+  return 'Basic ' + base64encode(username + ":" + password);
+}
+
+/**
+ * Parse an XML string into an `xml2js` document, asynchronously.
+ */
+function parseXML(s: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(s, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+/**
+ * Parse the first event from a calendar document.
+ *
+ * CalDAV gives us "singleton" calendars containing just one VEVENT, so
+ * this parses the ICS source as a calendar and then get the first (only)
+ * calendar in it.
+ */
+function parseEvent(ics: string) {
+  let cal = calendar.parse(ics);
+  for (let event of calendar.getEvents(cal)) {
+    return event;
+  }
+}
+
+/**
+ * Format a time for inclusion in an CalDAV query.
+ *
+ * This seems to be the "basic" format from ISO 8601. The full standard does
+ * not seem to be supported.
+ */
 function davtime(t: moment.Moment) {
   return t.format('YYYYMMDD[T]HHmmss[Z]');
 }
 
-function range_query(start: moment.Moment, end: moment.Moment) {
+/**
+ * Construct a CalDAV range query, which is an XML document, for getting the
+ * events between two times.
+ */
+function rangeQuery(start: moment.Moment, end: moment.Moment) {
   return `<?xml version="1.0" encoding="utf-8" ?>
 <C:calendar-query xmlns:D="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">
   <D:prop>
@@ -24,38 +77,10 @@ function range_query(start: moment.Moment, end: moment.Moment) {
 </C:calendar-query>`;
 };
 
-function base64encode(s: string) {
-  return new Buffer(s).toString('base64');
-}
-
-function basicauth(username: string, password: string): string {
-  return 'Basic ' + base64encode(username + ":" + password);
-}
-
-function parseXML(s: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    xml2js.parseString(s, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    });
-  });
-}
-
-function firstEvent(ics: string) {
-  let cal = calendar.parse(ics);
-  for (let event of calendar.getEvents(cal)) {
-    return event;
-  }
-}
-
 async function getSomeEvents(url: string, username: string, password: string)
 {
   let start = moment();
   let end = moment().add(7, 'days');
-  let query_xml = range_query(start, end);
 
   let res = await fetch(url, {
     method: 'REPORT',
@@ -64,7 +89,7 @@ async function getSomeEvents(url: string, username: string, password: string)
       'Authorization': basicauth(username, password),
       'User-Agent': 'opal/1.0.0',
     },
-    body: query_xml,
+    body: rangeQuery(start, end),
   });
   if (!res.ok) {
     throw "error communicating with CalDAV server";
@@ -78,7 +103,7 @@ async function getSomeEvents(url: string, username: string, password: string)
   // </multistatus>
   for (let response of data['multistatus']['response']) {
     let ics = response['propstat'][0]['prop'][0]['calendar-data'][0]['_'];
-    let event = firstEvent(ics);
+    let event = parseEvent(ics);
     if (event) {
       console.log(event.summary);
     }
