45af4d5b63705d5f6453434fe8838e674cba8ab2
Adrian Sampson <adrian@radbox.org>
2017-07-19 13:41:34 -0400

    Abstraction for individual authentications

--- multical/office.ts
+++ multical/office.ts
@@ -2,6 +2,7 @@ import * as oauth2 from 'simple-oauth2';
 import * as libweb from '../libweb';
 import * as http from 'http';
 import * as url from 'url';
+import * as crypto from 'crypto';
 
 /**
  * OAuth2 parameters for connecting to Office 365.
@@ -26,47 +27,86 @@ const SCOPES = [
  */
 const AUTH_ENDPOINT = '/authorize';
 
+/**
+ * Generate a random string for use as a token.
+ */
+async function randomString(bytes = 32): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    crypto.randomBytes(bytes, (err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buffer.toString('base64'));
+      }
+    });
+  });
+}
+
 export type TokenHandler = (token: oauth2.AccessToken) => void;
 
-/**
- * Create an HTTP route for handling Office 365 authentication.
- */
-export function authRoute(clientId: string, clientSecret: string,
-                          baseUrl: string, cbk: TokenHandler) {
-  let auth = oauth2.create({
-    client: { id: clientId, secret: clientSecret },
-    auth: OAUTH_PARAMS,
-  });
+export class Client {
+  readonly authRoute: libweb.Route;
+  readonly auth: oauth2.OAuthClient;
+  readonly callbackUrl: string;
+  readonly handlers = new Map<string, TokenHandler>();
 
-  let callbackUrl = url.resolve(baseUrl, AUTH_ENDPOINT);;
-  let authUrl = auth.authorizationCode.authorizeURL({
-    redirect_uri: callbackUrl,
-    scope: SCOPES.join(" "),
-  });
-  console.log(authUrl);
-
-  let route = new libweb.Route(AUTH_ENDPOINT, (req, res) => {
-    let code = libweb.query(req).get('code');
-    if (!code) {
-      throw 'no code received';
-    }
-    auth.authorizationCode.getToken({
-      code,
-      redirect_uri: callbackUrl,
-    }).then(t => {
-      let token = auth.accessToken.create(t);
-      cbk(token);
+  constructor(id: string, secret: string, baseUrl: string) {
+    this.auth = oauth2.create({
+      client: { id, secret },
+      auth: OAUTH_PARAMS,
     });
-    res.end('ok');
-  });
 
-  return route;
+    this.callbackUrl = url.resolve(baseUrl, AUTH_ENDPOINT);
+
+    this.authRoute = new libweb.Route(AUTH_ENDPOINT, (req, res) => {
+      let query = libweb.query(req);
+      let code = query.get('code');
+      if (!code) {
+        throw 'no code received';
+      }
+
+      this.auth.authorizationCode.getToken({
+        code,
+        redirect_uri: this.callbackUrl,
+      }).then(t => {
+        console.log(t);
+        let token = this.auth.accessToken.create(t);
+        this.authenticated(query.get('state'), token);
+      });
+      res.end('ok');
+    });
+  }
+
+  async authenticate(cbk: TokenHandler) {
+    let state = await randomString();
+    this.handlers.set(state, cbk);
+
+    return this.auth.authorizationCode.authorizeURL({
+      redirect_uri: this.callbackUrl,
+      scope: SCOPES.join(" "),
+      state,
+    });
+  }
+
+  authenticated(state: string | null, token: oauth2.AccessToken) {
+    if (!state) {
+      return;
+    }
+    let cbk = this.handlers.get(state);
+    if (cbk) {
+      cbk(token);
+    }
+  }
 }
 
 // Smoke test.
-let r = authRoute(process.argv[2], process.argv[3],
-                  'http://localhost:8191', token => {
-  console.log('got a token yo');
-});
-let server = http.createServer(libweb.dispatch([r]));
-server.listen(8191);
+async function main() {
+  let c = new Client(process.argv[2], process.argv[3], 'http://localhost:8191');
+  let server = http.createServer(libweb.dispatch([c.authRoute]));
+  server.listen(8191);
+  let u = await c.authenticate(token => {
+    console.log("got that token fam");
+  });
+  console.log(u);
+}
+main();
